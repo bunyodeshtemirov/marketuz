309f078d9a1393745ea46f58958550f3dc0adcdb
bunyodeshtemirov <84260246+bunyodeshtemirov@users.noreply.github.com>
2021-05-28 10:41:48 +0500

    second commit

--- marketuz/settings.py
+++ marketuz/settings.py
@@ -121,7 +121,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-MEDIA_URL = '/images/'
+STATIC_ROOT = BASE_DIR / 'static'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
